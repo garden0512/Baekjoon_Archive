ee7a0d1f186f2f8ad1956e697a3d56bd9111304d
정원 <88885421+garden0512@users.noreply.github.com>
2025-01-13 21:45:28 +0900

    1546번까지 품
    클래스2의 3문제만 일단 업로드

--- baekjoon_python.py
+++ baekjoon_python.py
@@ -1,1 +1,30 @@
-print("곧 양치하러 감")
+# 15829번
+# l = int(input())
+# stri = str(input())
+# sum = 0
+# for i in range(l):
+#     sum += (ord(stri[i]) - 96) * (31**i)
+# sum = sum % 1234567891
+# print(sum)
+
+# 1259번
+# i = 1
+# while True:
+#     i = int(input())
+#     if i == 0:
+#         break
+#     else:
+#         revi = int(str(i)[::-1])
+#         if i == revi:
+#             print("yes")
+#         else:
+#             print("no")
+
+# # 1546번
+# total = 0
+# sub_n = int(input())
+# score = list(map(int, input().split()))
+# maxi = max(score)
+# for i in score:
+#     total += (i / maxi) * 100
+# print(total / sub_n)
